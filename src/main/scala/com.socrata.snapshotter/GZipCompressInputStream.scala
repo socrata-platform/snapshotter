3eff4aa0b08eb86cf770e064f13ca591b1000d17
Cate Uselton <cathode.use@gmail.com>
2015-11-05 13:26:41 -0800

    Add GZipCompressInputStream class
    takes an input stream and returns a input stream producing the compressed version of that data

---
+++ src/main/scala/com.socrata.snapshotter/GZipCompressInputStream.scala
@@ -0,0 +1,69 @@
+package com.socrata.snapshotter
+
+import java.io.{PipedOutputStream, PipedInputStream, IOException, InputStream}
+import java.util.zip.GZIPOutputStream
+import com.rojoma.simplearm.v2.using
+
+class GZipCompressInputStream(val underlying: InputStream, pipeBufferSize: Int) extends InputStream {
+  private val worker: Worker = new Worker(underlying, pipeBufferSize)
+  worker.start()
+
+  override def read() = worker.read()
+
+  override def read(bytes: Array[Byte], off: Int, len: Int): Int = {
+    worker.read(bytes, off, len)
+  }
+
+  override def close: Unit = {
+    worker.shutdown()
+    underlying.close()
+  }
+
+  private class Worker(val in: InputStream, val pipeBufferSize: Int) extends Thread {
+    setDaemon(true)
+    setName("Compression thread")
+    val ReadFinished = -1
+    val compressed = new PipedInputStream(pipeBufferSize)
+    val pipe = new PipedOutputStream
+    var pendingException: Option[IOException] = None
+
+    pipe.connect(compressed)
+
+    // in -> compressor -> pipe -> compressed
+    override def run(): Unit = {
+      try {
+        using(new GZIPOutputStream(pipe, pipeBufferSize)) { compressor =>
+          val buffer: Array[Byte] = new Array(4096)
+          var count: Int = in.read(buffer)
+
+          while (count != ReadFinished) {
+            compressor.write(buffer, 0, count)
+            count = in.read(buffer)
+          }
+        }
+      } catch {
+        case e: IOException => pendingException = Some(e)
+      }
+    }
+
+    def read(): Int = {
+      val res = compressed.read()
+      if (res == ReadFinished && pendingException.isDefined) throw pendingException.get
+      res
+    }
+
+    def read(bytes: Array[Byte], off: Int, len: Int): Int = {
+      val res = compressed.read(bytes, off, len)
+      if (res == ReadFinished && pendingException.isDefined) throw pendingException.get
+      res
+    }
+
+
+    def shutdown(): Unit = {
+      compressed.close()
+      in.close()
+    }
+  }
+}
+
+
