0e81804eaeb812a2b78999fd699445fc372d8918
Cate Uselton <cathode.use@gmail.com>
2015-11-11 11:31:56 -0800

    List Service takes datasetId and returns json list

--- src/main/scala/com.socrata.snapshotter/BlobStoreManager.scala
+++ src/main/scala/com.socrata.snapshotter/BlobStoreManager.scala
@@ -4,19 +4,23 @@ import java.io.InputStream
 
 import com.amazonaws.event.{ProgressListener, ProgressEvent}
 import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.model.{AmazonS3Exception, ObjectMetadata, PutObjectRequest}
+import com.amazonaws.services.s3.model._
 import com.amazonaws.services.s3.transfer.TransferManager
 import com.amazonaws.services.s3.transfer.model.UploadResult
 
 import com.rojoma.json.v3.interpolation._
-import com.rojoma.json.v3.ast.JValue
+import com.rojoma.json.v3.ast.{JValue, JString}
 
 import org.slf4j.LoggerFactory
+
+import scala.collection.JavaConverters._
 
 object BlobStoreManager {
   private lazy val s3client = new AmazonS3Client()
   private lazy val manager = new TransferManager(s3client)
   private val logger = LoggerFactory.getLogger(getClass)
+  private val datasetIdLength = 9
+  private val fileExtensionLength = 7 //.csv.gz
 
   def shutdownManager(): Unit = {
     logger.info("Shutting down transfer manager.")
@@ -45,12 +49,38 @@ object BlobStoreManager {
 
   }
 
+  // TODO: account for possibility of truncated results (although not a problem in testing, as first 1000 results return)
+  def listObjects(bucketName: String, path: String): JValue = {
+    logger.info("Requesting a list...")
+    s3client.listObjects(SnapshotterConfig.awsBucketName, path)
+    val req = new ListObjectsRequest().withBucketName(bucketName).withPrefix(s"$path")
+    val objectListing = s3client.listObjects(req)
+    val objectSummaries: Seq[S3ObjectSummary] = objectListing.getObjectSummaries.asScala
+
+    val snapshots: Seq[JValue] =
+      objectSummaries.map( sum => {
+        logger.info(s"key: ${sum.getKey}")
+        val nameDate = parseKey(sum.getKey)
+        json"""{ name: ${nameDate._1},
+                 date: ${nameDate._2},
+                 size: ${sum.getSize} }"""})
+
+    json"""{datasetId: $path, count: ${snapshots.length}, snapshots: $snapshots }"""
+  }
+
+  def parseKey(keyName: String): (String, String) = {
+    val datasetId = keyName.slice(0,datasetIdLength)
+    val timestamp = keyName.slice(datasetIdLength + 1, keyName.length - fileExtensionLength - 1 )
+    (datasetId, timestamp)
+  }
+
   class LoggingListener(private val logger: org.slf4j.Logger) extends ProgressListener {
     def progressChanged(event: ProgressEvent): Unit = {
       if (event.getEventType == com.amazonaws.event.ProgressEventType.TRANSFER_COMPLETED_EVENT) {
         logger.info("Upload complete.")
       }
       logger.info(s"Transferred bytes: ${event.getBytesTransferred}")
+      logger.info(s"Progress event type: ${event.getEventType}")
     }
   }
 
---
+++ src/main/scala/com.socrata.snapshotter/ListService.scala
@@ -0,0 +1,36 @@
+package com.socrata.snapshotter
+
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.{S3ObjectSummary, ListObjectsRequest}
+import com.rojoma.json.v3.ast.JValue
+import com.socrata.curator.CuratedServiceClient
+import com.socrata.http.server.{HttpResponse, HttpRequest, HttpService}
+
+import com.socrata.http.server.responses._
+import com.socrata.http.server.implicits._
+import com.socrata.http.server.routing.SimpleResource
+
+import org.slf4j.LoggerFactory
+
+
+
+case class ListService(client: CuratedServiceClient) extends SimpleResource {
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  def service(datasetId: String): HttpService = {
+    new SimpleResource {
+      override def get: HttpService = req =>
+        handleRequest(req, datasetId)
+      }
+    }
+
+  def handleRequest(req: HttpRequest, datasetId: String): HttpResponse = {
+    val resp = requestList(datasetId)
+    OK ~> Content("application/json", s"$resp" )
+  }
+
+  def requestList(datasetId: String): JValue = {
+    BlobStoreManager.listObjects(SnapshotterConfig.awsBucketName, datasetId)
+  }
+
+}
