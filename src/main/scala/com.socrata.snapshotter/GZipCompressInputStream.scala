80013c28a9fecef71f98a4c7c6f8f81b692b830a
Cate Uselton <cathode.use@gmail.com>
2015-11-12 16:11:18 -0800

    GzipCompressInputStream read(byte[]) improvements
    read(Array(Byte),off,len)) will completely fill to specified len if possible
    helps to satisfy s3 minimum multi-part upload size restraints

--- src/main/scala/com.socrata.snapshotter/GZipCompressInputStream.scala
+++ src/main/scala/com.socrata.snapshotter/GZipCompressInputStream.scala
@@ -4,9 +4,12 @@ import java.io.{PipedOutputStream, PipedInputStream, IOException, InputStream}
 import java.util.zip.GZIPOutputStream
 
 import com.rojoma.simplearm.v2.using
+import org.slf4j.LoggerFactory
 
 class GZipCompressInputStream(val underlying: InputStream, pipeBufferSize: Int) extends InputStream {
   private val worker: Worker = new Worker(underlying, pipeBufferSize)
+  private val logger = LoggerFactory.getLogger(getClass)
+
   worker.start()
 
   override def read(): Int = worker.read()
@@ -53,12 +56,23 @@ class GZipCompressInputStream(val underlying: InputStream, pipeBufferSize: Int) 
       res
     }
 
-    def read(bytes: Array[Byte], off: Int, len: Int): Int = {
-      val res = compressed.read(bytes, off, len)
-      if (res == ReadFinished && pendingException.isDefined) throw pendingException.get
-      res
-    }
+    def read(bytes: Array[Byte], baseOff: Int, len: Int): Int = {
+      logger.info(s"Read called with length of: $len/${bytes.length}")
 
+      var byte = 0
+      var i = baseOff
+
+      while (i < len && byte != ReadFinished) {
+        byte = read()
+        if (byte != ReadFinished) {
+          bytes(i) = byte.toByte
+          i += 1
+        }
+      }
+
+      logger.info(s"Returning read after having read: ${i - baseOff}")
+      if (byte == ReadFinished && i == baseOff) ReadFinished else i - baseOff
+    }
 
     def shutdown(): Unit = {
       compressed.close()
