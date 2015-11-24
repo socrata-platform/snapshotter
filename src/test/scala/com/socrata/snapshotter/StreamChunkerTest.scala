919d7e2dee8251d5e6ab27fa4c07a6442090d3a7
Cate Uselton <cathode.use@gmail.com>
2015-11-24 14:48:22 -0800

    Add SingleByteInputStream class for testing

--- src/test/scala/com/socrata/snapshotter/StreamChunkerTest.scala
+++ src/test/scala/com/socrata/snapshotter/StreamChunkerTest.scala
@@ -5,60 +5,70 @@ import java.io.{ByteArrayOutputStream, ByteArrayInputStream}
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{MustMatchers, FunSuite}
 
+import scala.util.Random
+
 class StreamChunkerTest extends FunSuite with MustMatchers with MockitoSugar {
 
   // give input & buffer size in MB
-  private def getChunker(inputSize: Int, bufferSize: Int): StreamChunker = {
-    val bais = new ByteArrayInputStream(new Array[Byte](inMB(inputSize)))
-    new StreamChunker(bais, inMB(bufferSize))
+  private def getChunker(inputSize: Int, bufferSize: Int): (StreamChunker, Array[Byte]) = {
+    val inputBytes = new Array[Byte](inputSize)
+    Random.nextBytes(inputBytes)
+    val inStream = new SingleByteStream(inputBytes)
+    (new StreamChunker(inStream, bufferSize), inputBytes)
   }
 
   private def countChunks(sc: StreamChunker): Int = {
     sc.foldLeft (0) ((sum, _) => sum + 1)
   }
 
-  private def inMB(num: Int): Int = {
-    num * 1024 * 1024
-  }
-
   test("Returns divided input streams") {
-    assert(countChunks(getChunker(21, 8)) == 3)
+    val (sc, _) = getChunker(21, 8)
+    assert(countChunks(sc) == 3)
   }
 
   test("an input stream smaller than buffer size results in single input stream the same size as original input") {
-    val sc = getChunker(3,8)
+    val (sc, _) = getChunker(3,8)
     val count = sc.chunks.foldLeft(0) { (count, chunk) =>
       assert(chunk.partNumber == 1)
-      assert(chunk.size == inMB(3))
+      assert(chunk.size == 3)
       count + 1
     }
     assert(count == 1)
   }
 
   test("each chunked output stream is the same size as the buffer setting, when possible") {
-    val sc = getChunker(8,2)
-    sc.chunks.foreach { chunk => assert(chunk.size == inMB(2)) }
+    val (sc, _) = getChunker(8,2)
+    sc.chunks.foreach { chunk => assert(chunk.size == 2) }
   }
 
   test("size field returned is accurate for the contents of the matching inputStream") {
-    val sc = getChunker(4,2)
+    val (sc, _) = getChunker(4,2)
     sc.chunks.foreach { chunk =>
       // don't do this at home, kids
       val ba = Stream.continually(chunk.inputStream.read).takeWhile(_ != -1).map(_.toByte).toArray
-      assert(ba.length == inMB(2))
+      assert(ba.length == 2)
     }
   }
 
   test("input stream chunks combine to form the same original input data") {
-    val input = "Ordering test for a string chunked into input streams"
-    val inBytes = input.getBytes
-    val bais = new ByteArrayInputStream(inBytes)
-    val sc = new StreamChunker(bais, inBytes.length)
+    val (sc, inputBytes) = getChunker(20, 6)
 
     val outBytes = sc.chunks.foldLeft(new Array[Byte](0)) { (outBytes, chunk) =>
       outBytes ++ Stream.continually(chunk.inputStream.read).takeWhile(_ != -1).map(_.toByte)
     }
-    val output = new String(outBytes)
-    assert(output == input)
+    assert(outBytes.sameElements(inputBytes))
   }
+
+  class SingleByteStream(val bytes: Array[Byte]) extends ByteArrayInputStream(bytes) {
+    override def read(output: Array[Byte], offset: Int, len: Int): Int = {
+      val byte = this.read()
+      if (byte == -1) {
+        byte
+      } else {
+        output(offset) = byte.toByte
+        1
+      }
+    }
+  }
+
 }
