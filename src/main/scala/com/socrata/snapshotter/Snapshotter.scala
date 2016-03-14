6fe7d3c2ffc96b1c2b14c731795fb7de0080887c
Robert J. Macomber <robert.macomber@socrata.com>
2016-03-14 10:44:26 -0700

    Switch to standard directory layout

--- src/main/scala/com/socrata/snapshotter/Snapshotter.scala
+++ src/main/scala/com/socrata/snapshotter/Snapshotter.scala
@@ -4,7 +4,9 @@ import com.typesafe.config.ConfigFactory
 import java.util.concurrent.Executors
 import com.socrata.curator.{DiscoveryFromConfig, CuratorFromConfig}
 import com.socrata.http.server.SocrataServerJetty
+import com.socrata.http.common.AuxiliaryData
 import com.socrata.http.client.HttpClientHttpClient
+import com.socrata.http.server.curator.CuratorBroker
 import com.rojoma.simplearm.v2._
 
 object Snapshotter extends App {
@@ -14,11 +16,9 @@ object Snapshotter extends App {
       executor <- managed(Executors.newCachedThreadPool())
       httpClient <- managed(new HttpClientHttpClient(executor, HttpClientHttpClient.defaultOptions.withUserAgent("snapshotter")))
       curator <- CuratorFromConfig(config.curator)
-      discovery <- DiscoveryFromConfig(classOf[Void], curator, config.discovery)
-      // broker is loaded with our service's config info and is able to create a connection with zookeeper
-      // client (returned from zookeeper) is configured for making requests to core (specified in config file)
-      client <- broker.clientFor(config.client)
+      discovery <- DiscoveryFromConfig(classOf[AuxiliaryData], curator, config.discovery)
     } {
+      val coreProvider = CuratorServerProvider(httpClient, serviceProvider
       val broker = new CuratorBroker(discovery, config.address.getHostAddress, config.service.advertisement.service, None)
       val snapshotService = SnapshotService(client)
       val router = Router(versionService = VersionService,
