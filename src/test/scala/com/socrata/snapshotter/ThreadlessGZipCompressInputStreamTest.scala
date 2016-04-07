aa49b0efbb4d589a1543075a69663b8e77156af4
Robert J. Macomber <robert.macomber@socrata.com>
2016-04-07 10:24:56 -0700

    Remove special case and a bit of commented-out code

--- src/test/scala/com/socrata/snapshotter/ThreadlessGZipCompressInputStreamTest.scala
+++ src/test/scala/com/socrata/snapshotter/ThreadlessGZipCompressInputStreamTest.scala
@@ -34,27 +34,6 @@ class ThreadlessGZipCompressInputStreamTest extends FunSuite with MustMatchers w
       }
   }
 
-  test("ThreadlessGZipCompressInputStream0 works") {
-    val bufsiz0 = 1
-    val input0: List[List[Byte]] = List(List(127))
-    locally {
-      val bufsiz = (bufsiz0 & 0xff) + 1
-      val input = new ChunkedInputStream(input0.map(_.toArray).map(new ByteArrayInputStream(_)))
-
-      val compressed = new ThreadlessGZipCompressInputStream(input, bufsiz)
-      val baos = new ByteArrayOutputStream
-      IOUtils.copy(compressed, baos)
-      compressed.close()
-
-      val gzis = new GZIPInputStream(new ByteArrayInputStream(baos.toByteArray))
-      val target = input0.flatten.toArray
-      val received = new Array[Byte](target.length)
-      IOUtils.readFully(gzis, received)
-      received must equal (target)
-      gzis.read() must be (-1)
-    }
-  }
-
   test("ThreadlessGZipCompressInputStream works") {
     forAll { (bufsiz0: Int, input0: List[List[Byte]]) =>
       val bufsiz = (bufsiz0 & 0xfff) + 1
@@ -71,7 +50,6 @@ class ThreadlessGZipCompressInputStreamTest extends FunSuite with MustMatchers w
       IOUtils.readFully(gzis, received)
       received must equal (target)
       gzis.read() must be (-1)
-      // an [EOFException] must be thrownBy (gzis.read()) // because gzipinputstream only lets you see its EOF once
     }
   }
 }
