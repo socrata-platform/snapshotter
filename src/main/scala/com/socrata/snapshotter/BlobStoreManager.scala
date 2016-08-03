627dda2d1067cd1ca94a2c71b2ea28162a2f2d6a
George Matter <gmvi@users.noreply.github.com>
2016-08-03 16:50:13 -0700

    Revert "Implement DELETE verb for fetchSnapshotService"

--- src/main/scala/com/socrata/snapshotter/BlobStoreManager.scala
+++ src/main/scala/com/socrata/snapshotter/BlobStoreManager.scala
@@ -3,15 +3,16 @@ package com.socrata.snapshotter
 import java.io.{Closeable, InputStream}
 import java.util.Date
 
-import com.amazonaws.AmazonServiceException
-import org.joda.time.{DateTime, DateTimeZone}
+import org.joda.time.{DateTimeZone, DateTime}
 
 import scala.collection.JavaConverters._
-import com.amazonaws.event.{ProgressEvent, ProgressListener}
+
+import com.amazonaws.event.{ProgressListener, ProgressEvent}
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model._
 import com.amazonaws.services.s3.transfer.TransferManager
 import com.amazonaws.services.s3.transfer.model.UploadResult
+import com.rojoma.json.v3.interpolation._
 import com.rojoma.json.v3.ast.JValue
 import com.rojoma.simplearm.v2._
 import org.slf4j.LoggerFactory
@@ -32,7 +33,7 @@ class BlobStoreManager(bucketName: String, uploadPartSize: Int) extends Closeabl
       try {
         op
       } catch {
-        case e: AmazonServiceException if e.getStatusCode == 503 || e.getStatusCode == 500 =>
+        case e: AmazonS3Exception if e.getStatusCode == 503 || e.getStatusCode == 500 =>
           // sometimes AWS returns 503s or 500s for perfectly good requests; we'll
           // treat them as transient failures and retry the opeartion
           if(retryCount < retryLimit) {
@@ -60,7 +61,7 @@ class BlobStoreManager(bucketName: String, uploadPartSize: Int) extends Closeabl
       logger.debug(s"uploadResult: $uploadResult")
       Right(uploadResult)
     } catch {
-      case exception: AmazonServiceException => Left(
+      case exception: AmazonS3Exception => Left(
         json"""{ message: "Problem uploading to S3",
                      error: ${exception.toString},
                      "error code": ${exception.getErrorCode},
@@ -91,7 +92,7 @@ class BlobStoreManager(bucketName: String, uploadPartSize: Int) extends Closeabl
                 new CompleteMultipartUploadRequest(bucketName, path, uploadId, uploadPartTags))
             })
     } catch {
-      case exception: AmazonServiceException =>
+      case exception: AmazonS3Exception =>
         val msg =
           json"""{ message: "Problem uploading to S3",
                        error: ${exception.toString},
@@ -107,13 +108,9 @@ class BlobStoreManager(bucketName: String, uploadPartSize: Int) extends Closeabl
     try {
       Some(resourceScope.open(retrying(s3client.getObject(bucketName, path))))
     } catch {
-      case e: AmazonServiceException if e.getStatusCode == 404 =>
+      case e: AmazonS3Exception if e.getStatusCode == 404 =>
         None
     }
-
-  def delete(path: String, resourceScope: ResourceScope) =
-    // s3client.deleteObject never returns NotFound (thus providing idempotency)
-    retrying(s3client.deleteObject(bucketName, path))
 
   def sendRequests(partReqs: Iterator[UploadPartRequest]): List[PartETag] = {
     partReqs.map { req =>
